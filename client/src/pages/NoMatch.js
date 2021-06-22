a935ea5bf0e20f6a8d5ccddcd5d51332ae58ae8c
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-06-21 22:02:28 -0400

    added nomatch page content

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+function NoMatch() {
+    return (
+        <>
+            <Header>
+                <h1>404 page not found</h1>
+                {/* add 404 image here */}
+            </Header>
+        </>
+    );
+};
+
+export default NoMatch;
