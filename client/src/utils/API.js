3c00ba8aa3ef7f9e732a50d9bf01effa2f6d6a69
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-06-21 22:28:16 -0400

    added additional functions to utils/api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,4 +4,16 @@ export default {
     getBook: function (query) {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
     },
+
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id).then(result => result.data);
+    },
+
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData).then(result => result.data);
+    },
+
+    savedBooks: function () {
+        return axios.get("/api/books").then(result => result.data);
+    },
 };
